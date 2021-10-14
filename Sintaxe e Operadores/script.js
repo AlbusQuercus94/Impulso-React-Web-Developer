22f934c077456d1288f0828cb83fa7fdbe5cfb56
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-10-13 22:33:05 -0300

    Criação Sintaxe e Operadores
    Conclusão da aula de Sintaxe e operadores.

---
+++ Sintaxe e Operadores/script.js
@@ -0,0 +1,56 @@
+// -------------------------------------------------------
+// Parte 1 - Atribuição
+/*
+OPERADOR       EXEMPLO       EQUIVALENTE
+   =           X = Y         X = Y      //Apenas um símbolo de igual tem o sentido de ATRIBUIÇÃO
+   +=          X += Y        X = X + Y
+   -=          X -= Y        X = X - Y
+   *=          X *= Y        X = X * Y
+   /=          X /= Y        X = X / Y
+   %=          X %= Y        X = X % Y //Porcentagem entre dois números tem o sentido de resto da divisão.
+*/
+
+// -------------------------------------------------------
+// Parte 2 - Aritimética
+/*
+OPERADOR        DESCRIÇÃO
+   +            Adição
+   -            Subtração
+   *            Multiplicação
+   **           Exponenciação
+   /            Divisão
+   %            Resto de uma divisão
+   ++           Incrementar
+   --           Decrementar
+*/
+
+// -------------------------------------------------------
+// Parte 3 - Comparação
+/*
+OPERADOR        DESCRIÇÃO
+   =            Atribuição
+   ==           Igual
+   ===          Valor e tipo iguais
+   !=           Diferente
+   !==          Valor e tipo diferentes
+   >            Maior que
+   <            Menor que
+   >=           Maior ou igual
+   <=           Menor ou igual
+*/
+
+// -------------------------------------------------------
+// Parte 4 - Lógica
+/*
+OPERADOR        DESCRIÇÃO
+   &&           "e" lógica
+   ||           Ou
+   !            não lógico
+*/
+
+// -------------------------------------------------------
+// Parte 5 - If Ternário
+/*
+
+Condição    &    Se verdadeiro   &    Se falso
+*/
