ba6ded5758e2e82fa363e8b006e1cb2adf3c22a9
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-10-20 22:24:09 -0300

    Update Aula 03
    Conclusão da aula 03 de Funções.

--- Funções/script.js
+++ Funções/script.js
@@ -78,9 +78,89 @@ console.log(chave1, chave2, chave3) // Inves de e usar obj3.chave1, o desestruct
 // ----------------------------------------------------------------------
 //AULA 3 - FOR, WHILE E IF
 //Laços de Repetição
-//for
-//for...in: iteração para objetos
-//for...of: iteração para arrays
-//if/else if/else
+
+//if ... else: é um laço de repetição que recebe uma condição, e caso verdadeira, retorna um bloco, caso falsa, retorna outro bloco.
+/*
+if(true){// Caso verdadeiro
+    // Declaração se verdadeiro
+}else{// Caso falso
+    // Declaração caso falso
+}
+*/
+//Também há o caso de mais de uma condição, que é onde entra-se o else if
+/*
+if(true){
+    // Condição 1 retorna true
+} else if(!false){
+    // Condição 1 retorna falso, condição 2 retorna true
+    // Declaração retornada
+}else{
+    // Nenhuma das condições anteriores retornou true
+}
+*/
+//SWITCH : é um laço que retorna um determinado valor dependendo do resultado da condição
+
+let switch_test = 'olá';
+
+switch (switch_test){
+    case 'oi':
+        console.log('Prazer')
+        break
+    case 'olá':
+        console.log('Muito Prazer')
+        break
+    default:
+        console.log('Oi');
+        break
+}
+
+
+//for: é um laço que repete o mesmo bloco de código um determinado número de vezes. É separado em 4 partes.
+
+// for(/*Declaração 1*/; /*Declaração 2*/;/*Declaração 3*/){
+//     //Bloco
+// }
+//Declaração 1 é executada uma vez no primeiro laço
+//Declaração 2 é a condição para a repetição do laço
+//Declaração 3 é o código executado ao fim de cada laço
+
+//for...in: iteração para objetos, serve também para arrays, mas não se recomenda
+
+for (x in obj2){
+    console.log(x)
+}
+
+//Acima, para cada propriedade x em object, a propriedade será apresentada no console. O 'x' dentro do for não é como uma incognita, pode ter qualquer nome, retorna sempre as propriedades.
+
+//for...of: iteração para arrays. Possui uma sinxate parecida com o for in.
+for(x of arr2){
+    console.log(arr2.indexOf(x), x)
+}
 //while...do: enquanto uma condição for verdadeira executa um código até a condição se tornar falsa
-//do...while: executa um codigo enquanto uma função for verdadeira
+let i = 0;
+while(i<3){
+    i++
+    console.log('É true', i)
+}
+
+
+//do...while: executa um codigo antes e verifica a condição do while. Caso verdadeira repete o laço.
+do{
+    i++
+    console.log('É true antes', i)
+}while(i<3)
+
+
+// -------------------------------------------------------
+// AULA 4 - THIS
+// This: é uma palavra reservada que significa 'este'. Ela depende do contexto para fazer sentido. Dentro do contexto de um objeto, this é o mesmo que o nome do objeto. Exemplo: obj2.chave1 == this.chave1 (só funciona dentro do objeto)
+
+let obj3 = {
+    nome: 'Pedro',
+    sobrenome: 'Albuquerque',
+    completo: function (){
+        return this.nome + ' ' + this.sobrenome
+    }
+}
+
+console.log(obj3.completo())
