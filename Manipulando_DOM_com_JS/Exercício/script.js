a46f3f925eaf51eddf29c0b0061ed57da745bc5b
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-10-26 22:21:37 -0300

    Update Manipulando Dom
    Atualizando o exercício, colocando a troca do texto.

--- Manipulando_DOM_com_JS/Exercício/script.js
+++ Manipulando_DOM_com_JS/Exercício/script.js
@@ -3,10 +3,24 @@ let body = document.querySelector('body')
 let footer = document.querySelector('footer')
 
 
-function change(){
+function changeStyle(){
     button.classList.toggle('dark-mode');
     body.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
+}
+
+function changeText(){
+    if(button.outerText == 'Dark Mode'){
+        button.innerHTML = 'Light Mode'
+    }
+    else{
+        button.innerHTML = 'Dark Mode'        
+    }
+}
+
+function change(){
+    changeStyle();
+    changeText();
 }
 
 button.addEventListener('click',change)
