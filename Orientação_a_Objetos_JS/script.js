0317a1770ec36994420e941fcae8968937307696
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-01 09:23:47 -0300

    Create Orientação a Objeto JS
    Criação e conclusão da primeira aula sobre Paradigmas da Programação e sobre os Pilares da Orientação a Objeto

---
+++ Orientação_a_Objetos_JS/script.js
@@ -0,0 +1,31 @@
+// AULA 01 - PARADIGMAS DA PROGRAMAÇÃO
+//Imperativo: foca em como resolver os problemas
+/*
+    -Procedural;
+    -Orientação a Objeto;
+    -Processing Parallel;
+*/
+
+//Declarativo: foca em no que fazer
+/*
+    -Lógica;
+    -Functional/DataFlow;
+    -Database;
+*/
+
+//Aqui é uma aula de Orientação a objeto que é um paradigma imperativo.
+//Em Programação Orientada a Objeto (POO) "os programas são 'objetos' que possuem uma série de propriedades." Os pilares são:
+/*
+    Herança
+    Polimorfismo
+    Encapsulamento
+    Abstração
+*/
+
+//Abstração: Processo mental que consiste em isolar um aspecto determinado de um estade de coisas relativamente complexo, a fim de simplificar a sua avaliação0, classificação ou para permitir a comunicação do mesmo.
+
+//Herança: o objeto filho herda propriedades e métodos do objeto mãe.
+
+//Encapsulamento: cada classe tem propriedades e métodos independentes do restante do código.
+
+//Polimorfismo: objetos podem herdar a mesma classe masse, mas se comportarem de forma diferente quando invocamos seus métodos
