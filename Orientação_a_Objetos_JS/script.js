9fde29ba4c7f42a94f5a0f18d4ea28bb46043ffd
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-01 10:08:54 -0300

    Update Orientação a Objetos JS
    Conclusão da segunda aula sobre protótipos e classes.

--- Orientação_a_Objetos_JS/script.js
+++ Orientação_a_Objetos_JS/script.js
@@ -29,3 +29,66 @@
 //Encapsulamento: cada classe tem propriedades e métodos independentes do restante do código.
 
 //Polimorfismo: objetos podem herdar a mesma classe masse, mas se comportarem de forma diferente quando invocamos seus métodos
+
+
+// -------------------------------------------------------------------
+// AULA 02 - PROTOTIPOS E CLASSES
+
+//Protótipos: todos os objetos JavaScript herdam propriedades e métodos de um prototype. O objeto Object.prototype está no topo desta cadeia.
+
+
+//Classes: em JS não possuem classes nativamente. O que existe são "syntactic sugar", uma sintaxe feita para facilitar a escrita.
+//Em uma classe possuem os seguintes propriedades:
+/*
+    constructor
+    getter e setter
+    super
+    métodos
+*/
+
+// Exemplo:
+class Animal{
+    constructor(type = 'animal'){
+        this.type = type;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    set type(val){
+        this._type = val.toUpperCase();
+    }
+
+    //Método
+    makeSound(){
+        return 'Making an animal sound.'
+    }
+}
+
+class Cat extends Animal{
+    constructor(specie){
+        super('cat')
+        this.specie = specie
+    }
+
+    get specie(){
+        return this._specie
+    }
+
+    set specie(val){
+        this._specie = val
+    }
+
+    makeSound(){
+        let sound = super.makeSound()
+        return sound + ` Woof I'm a polyglot cat.`
+    }
+}
+let animal = new Animal()
+let siames = new Cat ('Siamês')
+
+siames._specie = 'Persa'
+
+console.log(animal)
+console.log(siames)
