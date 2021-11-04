9fb8781369f7afd47676e60afc9f158fa8d0f151
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-04 07:25:24 -0300

    Update Parte 6 Intro React JS
    Conclusão da parte 6 sobre listas e chaves.

--- React_JS/Introdução_ao_ReactJS/App_Tutorial_1/src/App.js
+++ React_JS/Introdução_ao_ReactJS/App_Tutorial_1/src/App.js
@@ -93,28 +93,52 @@ function parte_6(){
   const customers = [
     {
       id: 1,
-      name:'Garnet Dil'
+      name:'Garnet Dil',
+      job: "Summoner"
     },
     {
       id: 2,
-      name: 'Vivi'
+      name: 'Vivi',
+      job: "Black Mage"
     },
     {
       id: 3,
-      name: 'Zidane'
+      name: 'Zidane',
+      job: "Thief"
     },
     {
       id: 4,
-      name:'Steiner'
+      name:'Steiner',
+      job: "Knight"
     }
   ]
   /*Renderizando Multiplos Elementos Início */
+  /*Sempre separar a chamada em blocos e camadas de serviços. */
   function renderCustomers(customer,index){
     return (
-      <li>Customer: {customer.name} - Position: {index + 1}</li>
+      <div key={`"customer: ${customer.id}`} /*Chaves devem ser únicas entre elementos irmãos*/>
+        <li>
+          Customer: {customer.name} <br/>
+          Position: {index + 1} <br/>
+          Jobs: {customer.job} {/*Visivelmente mais agradável. */}
+        </li>
+        <br/>
+        {/*{renderJobs(customer, index)}{/*Feito na aula /}*/}
+      </div>
     )
   }
   /*Renderizando Multiplos Elementos Fim */
+
+  function renderJobs(customer,index){
+    return (
+      <li>
+        Jobs: {customer.job}
+        <br/>
+      </li>
+    )
+  }
+
+
   return(
     <div>
       <p>Aula de Renderização de Condicionais</p>
