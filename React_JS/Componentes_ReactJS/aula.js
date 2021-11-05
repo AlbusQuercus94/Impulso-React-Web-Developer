2a9a536ec4274197aa1bfdf772fa0c18aeb1077d
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-05 07:05:38 -0300

    Create Componentes React JS
    Criação da pasta e conclusão da primeira parte do módulo.

---
+++ React_JS/Componentes_ReactJS/aula.js
@@ -0,0 +1,7 @@
+// PARTE 1 - INSTALANDO O REACT JS
+//Nessa parte não adicionou muita coisa pois já tinha conhecimento prévio de instalação.
+//As coisas que eu aprendi aqui foram:
+/*
+    A pasta node modules pode ser excluida e readicionada através do comando npm i no terminal
+    O arquivo gitignore é um arquivo que mostra quais os arquivos e pastas não serão enviados para o github no push.
+*/
