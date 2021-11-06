fa208f3fa2bc7c6dfab99cf483cd672bd1c2ce43
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-06 19:53:02 -0300

    Create Estilização e Formulários

---
+++ React_JS/Estilização_e_Formulários_React_JS/aula.js
@@ -0,0 +1,27 @@
+// Parte 1 - Estilizando com CSS
+// Maneiras de estilização
+/*
+Inline: usando style dentro da tag.
+    Prós:
+        Maneira masi prática e direta
+        Ajustes rápidos
+        Testes de estilo
+    Contras:
+        Difícil manutenção
+Classes: usando arquivo CSS estilizando os elementos por identificação de classes.]
+    Prós:
+        Maneira mais prática e direta
+    Contras:
+        Difícil manutenção
+        Pouca Flexibilidade
+        Conflitos com Nomes
+CSS com JS: npm install --save styled-components
+    Prós:
+        Facilidade para remover CSS
+        Estilos dinâmicos
+*/
+
+// Parte 2 - Stateful vs Stateless
+
+//Stateful: componente usa state. Possui gerenciamento de estados no componente e são construidos usando classes em JS.
+//Stateless: componente não usa state
