1569e2539993dc1b63630ff89be872c841236e08
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-07 11:59:53 -0300

    Update Projeto
    Desenvolvimento do menu lateral colocando um input field e intalando o  material react text field.

--- React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/index.jsx
+++ React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/index.jsx
@@ -1,9 +1,29 @@
+import logo from "../../assets/logo.svg"
+import TextField, { Input } from "@material/react-text-field/dist/index";
 import React from "react";
+import { useState } from "react";
 import { Container } from "./styles";
+import { Search } from "./styles";
 
-const Home = () => 
+const Home = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    return(
         <Container>
-            Hello, World
+            <Search>
+                <img src={logo} alt="Logo_do_Restaurante" />
+                <TextField
+                label='Search'
+                outlined
+                // onTrailingIconSelect={() => this.setState({value: ''})}
+                //   trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+                >
+                    <Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
+                </TextField>
+            </Search>
         </Container>
-
+    )
+}
 export default Home
--- React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/styles.js
+++ React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/styles.js
@@ -3,7 +3,15 @@ import styled from "styled-components";
 export const Container = styled.aside
 `
     background-color: ${(props) => props.theme.colors.background};
-    width: 360px;
     height: 100vh;
     overflow-y: auto;
+    width: 360px;
 `;
+
+export const Search = styled.section `
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2%;
+`
