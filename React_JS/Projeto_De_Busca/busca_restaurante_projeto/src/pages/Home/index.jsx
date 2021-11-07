d3973f6352f7b9594934e977de375a3ec4899e38
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-07 18:37:54 -0300

    Update Projeto
    Finalizando a barra de procura e instalando o pacote de ícones.

--- React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/index.jsx
+++ React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/index.jsx
@@ -1,29 +1,32 @@
 import logo from "../../assets/logo.svg"
 import TextField, { Input } from "@material/react-text-field/dist/index";
+import MaterialIcon from "@material/react-material-icon/dist/index";
 import React from "react";
 import { useState } from "react";
-import { Container } from "./styles";
-import { Search } from "./styles";
+import * as Style from "./styles";
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('')
 
     return(
-        <Container>
-            <Search>
-                <img src={logo} alt="Logo_do_Restaurante" />
-                <TextField
-                label='Search'
-                outlined
-                // onTrailingIconSelect={() => this.setState({value: ''})}
-                //   trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-                >
-                    <Input
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)} />
-                </TextField>
-            </Search>
-        </Container>
+        <Style.Wrapper>
+            <Style.Container>
+                <Style.Search>
+                    <Style.Logo src={logo} alt="Logo_do_Restaurante" />
+                    <TextField
+                    label='Search'
+                    outlined
+                    // onTrailingIconSelect={() => this.setState({value: ''})}
+                    trailingIcon={<MaterialIcon role="button" icon="search"/>}
+                    >
+                        <Input
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                </Style.Search>
+            </Style.Container>
+            <Style.Map/>
+        </Style.Wrapper>
     )
 }
 export default Home
--- React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/styles.js
+++ React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/pages/Home/styles.js
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
+
 export const Container = styled.aside
 `
-    background-color: ${(props) => props.theme.colors.background};
-    height: 100vh;
-    overflow-y: auto;
-    width: 360px;
+background-color: ${(props) => props.theme.colors.background};
+height: 100vh;
+overflow-y: auto;
+width: 360px;
 `;
 
+export const Logo = styled.img`
+margin-bottom: 3%;
+`
+export const Map = styled.div`
+    background-color: pink;
+    width: 100%
+`
+
 export const Search = styled.section `
-    background-color: #fff;
+background-color: #fff;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 2%;
+`
+
+export const Wrapper = styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 2%;
 `
