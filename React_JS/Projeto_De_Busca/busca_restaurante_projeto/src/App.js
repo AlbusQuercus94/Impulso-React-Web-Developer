a71168020782dcb86b7a9f97bd677c308113ad3f
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-07 11:31:01 -0300

    Update Projeto
    Instalação de styled-reset

--- React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/App.js
+++ React_JS/Projeto_De_Busca/busca_restaurante_projeto/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
 import './App.css';
-import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset'
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Reset/>
       <Home />
     </ThemeProvider>
   );
